ec41542d7294d671ae37d6244b5cb289124e3297
prabhakarthiru <prabhakarthiru@gmail.com>
2019-11-29 13:27:07 +1100

    update packer.sh

---
+++ packer.sh
@@ -0,0 +1,23 @@
+# packer script
+
+source cicd/params.env
+source cicd/assumerole.env
+
+
+TASK=$1
+
+  case $TASK in
+	validate)
+		echo "Starting Packer validate"
+        packer validate $PACKER_INPUT_JSON
+		;;
+	build)
+		echo "starting packer build"
+        packer build $PACKER_INPUT_JSON
+		break
+		;;
+	*)
+		echo "Packer defined to execute only validate, build"
+        echo "Skipping packer since the input didn't match with requirement"
+		;;
+  esac
